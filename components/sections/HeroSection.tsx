5fd2fb39a4e79d11bb7f6cf4a3158e7b50ad9a50
kovshovsavva <kovshovsav@gmail.com>
2026-07-15 18:43:12 +0300

    перенос металоконструкции

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { IconArrowRight } from "@/components/icons";
 
-const heroLines = ["РЕЗЕРВУАРЫ", "ДЫМОВЫЕ ТРУБЫ", "МЕТАЛЛОКОНСТРУКЦИИ"];
+const heroLines = ["РЕЗЕРВУАРЫ", "ДЫМОВЫЕ ТРУБЫ", "МЕТАЛЛО-", "КОНСТРУКЦИИ"];
 
 export function HeroSection() {
   return (
